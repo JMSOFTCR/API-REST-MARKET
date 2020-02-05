770ac61dc2ae1afb1e5c9e8ca7364148e78e3401
thanatos9516 <chepe352013@gmail.com>
2020-02-05 00:38:32 -0600

    update

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,16 @@ import { createPool, Pool } from "mysql2/promise";
 
 export async function connect(): Promise<Pool>{
     const connection = await createPool({
-        host: 'localhost',
-        user: 'root',
-        database: 'db_sistema',
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b177a2417b890a',
+        password: 'd6ca3f29',
+        database: 'heroku_3fba89bbdcdc706',
         connectionLimit: 10
     })
     return connection;
 }
+
+/* host: 'localhost',
+user: 'root',
+database: 'db_sistema',
+connectionLimit: 10 */
